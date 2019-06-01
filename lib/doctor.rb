ab221cb15a2cca1c265e0cef0663ee31e1b3f3dd
M Banks <montensia1@outlook.com>
2019-06-01 22:43:58 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -18,7 +18,7 @@ class Doctor
   def appointments
     Appointment.all.select do |selection|
       #binding.pry
-      selection.doctor.name == self
+      selection.doctor == self
     end
   end
 end
